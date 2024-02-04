fed2c98a4e70ded2e3bf46e96f189f3cc2643f97
MishaVorozhbyt <mykhailo.vorozhbyt@asta.mobi>
2024-02-04 22:30:18 +0200

    [16] - fixed gitignore and up libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version "1.6.10-1.0.4" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
 tasks.register("clean", Delete::class){
